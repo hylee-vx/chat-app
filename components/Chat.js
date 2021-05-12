a44fc3d4c0d4cf09a6441c838d985aa3537c210f
Hyun Young Lee <hylee.v1@gmail.com>
2021-05-12 11:43:39 +0100

    Add Gifted Chat library to handle UI for Chat screen
    - convert Chat.js to class component to follow CF5.2 material
    - customise message bubble colour
    - add Android keyboard fix for writing messages with KeyboardAvoidingView
    **TO FIX** need to add iOs keyboard fix for Start screen — keyboard hides most of inputContainer when entering name

--- components/Chat.js
+++ components/Chat.js
@@ -1,29 +1,81 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 
-const Chat = props => {
-    const { name, backgroundColour } = props.route.params;
-    props.navigation.setOptions({ title: name });
+class Chat extends Component {
+    state = {
+        messages: []
+    };
 
-    return (
-        <View style={[styles.container, { backgroundColor: backgroundColour }]}>
-            <Text style={styles.text}>
-                What's on your mind?
-            </Text>
-        </View>
-    );
+    componentDidMount() {
+        const { name } = this.props.route.params;
+        this.setState({
+            messages: [
+                {
+                    _id: 1,
+                    text: 'Hello developer',
+                    createdAt: new Date(),
+                    user: {
+                        _id: 2,
+                        name: 'React Native',
+                        avatar: 'https://placeimg.com/140/140/any',
+                    },
+                },
+                {
+                    _id: 2,
+                    text: `${name} has entered the chat`,
+                    createdAt: new Date(),
+                    system: true,
+                },
+            ],
+        });
+    }
+
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages)
+        }));
+    }
+
+    renderBubble(props) {
+        return (
+            <Bubble
+                {...props}
+                wrapperStyle={{
+                    right: {
+                        backgroundColor: '#008080'
+                    },
+                }}
+            />
+        );
+    }
+
+    render() {
+        const { name, backgroundColour } = this.props.route.params;
+        this.props.navigation.setOptions({ title: name });
+
+        return (
+            <View style={[styles.container, { backgroundColor: backgroundColour }]}>
+                <GiftedChat
+                    renderBubble={this.renderBubble.bind(this)}
+                    messages={this.state.messages}
+                    onSend={messages => this.onSend(messages)}
+                    user={{
+                        _id: 1
+                    }}
+                />
+                {/* Android keyboard fix */}
+                {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
+                }
+            </View>
+        );
+    }
 };
 
 export default Chat;
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        alignItems: 'center'
-    },
-    text: {
-        fontSize: 16,
-        fontWeight: '600',
-        marginTop: 30
+        flex: 1
     }
 })
