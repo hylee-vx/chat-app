e7c46328465d2fd52c81db18ef10399e241793a0
Hyun Young Lee <hylee.v1@gmail.com>
2021-05-13 13:45:17 +0100

    Integrate Firebase database with chat app to store messages
    - authenticates user, adds new messages to DB and pushes DB changes to client side
    - users with access to all messages
    **TO ADD** system message with `${name} has entered the chat` for each user
    **TO FIX** change name at top of chat screen - should be the name of person you are talking to, not own name
    **TO FIX** delete hardcoded default message - able to set up document without any values?

--- components/Chat.js
+++ components/Chat.js
@@ -1,42 +1,119 @@
 import React, { Component } from 'react';
 import { StyleSheet, View, Platform, KeyboardAvoidingView } from 'react-native';
 import { Bubble, GiftedChat } from 'react-native-gifted-chat';
+import firebase from 'firebase';
+import firestore from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAlXPIwPwUOmUrOqN8bk2ZN_juX8Hs_IBU",
+    authDomain: "chat-app-19070.firebaseapp.com",
+    projectId: "chat-app-19070",
+    storageBucket: "chat-app-19070.appspot.com",
+    messagingSenderId: "287333815027",
+    appId: "1:287333815027:web:b1685e06d0052180efdd86",
+};
 
 class Chat extends Component {
-    state = {
-        messages: []
-    };
+    constructor(props) {
+        super(props);
+        this.state = {
+            messages: [],
+            uid: 0,
+            user: {
+                _id: '',
+                name: '',
+                avatar: '',
+            },
+        };
+        // initialises connection to Firebase DB
+        if (!firebase.apps.length) {
+            firebase.initializeApp(firebaseConfig);
+        }
+        // reference to all messages in DB
+        this.referenceChatMessages = firebase.firestore().collection('messages');
+    }
 
     componentDidMount() {
+        // adds user name to header
         const { name } = this.props.route.params;
-        this.setState({
-            messages: [
-                {
-                    _id: 1,
-                    text: 'Hello developer',
-                    createdAt: new Date(),
+        this.props.navigation.setOptions({ title: name });
+
+        // authenticates user with firebase
+        this.authUnsubscribe = firebase.auth()
+            .onAuthStateChanged(async (user) => {
+                if (!user) {
+                    await firebase.auth().signInAnonymously();
+                }
+
+                this.setState({
+                    uid: user.uid,
                     user: {
-                        _id: 2,
-                        name: 'React Native',
+                        _id: user.uid,
+                        name: name,
                         avatar: 'https://placeimg.com/140/140/any',
                     },
+                    messages: [],
+                });
+
+                // listens for changes to DB
+                this.unsubscribeChatUser = this.referenceChatMessages
+                    .orderBy('createdAt', 'desc')
+                    .onSnapshot(this.onCollectionUpdate);
+            });
+    }
+
+    // unsubscribes from user authentication and DB updates
+    componentWillUnmount() {
+        this.authUnsubscribe();
+        this.unsubscribeChatUser();
+    }
+
+    // updates messages on client-side when new message added to DB
+    onCollectionUpdate = querySnapshot => {
+        const messages = [];
+        // goes through each document
+        querySnapshot.forEach(doc => {
+            // gets QueryDocumentSnapshot's data
+            let data = doc.data();
+            messages.push({
+                _id: data._id,
+                text: data.text,
+                createdAt: data.createdAt.toDate(),
+                user: {
+                    _id: data.user._id,
+                    name: data.user.nme,
+                    avatar: data.user.avatar,
                 },
-                {
-                    _id: 2,
-                    text: `${name} has entered the chat`,
-                    createdAt: new Date(),
-                    system: true,
-                },
-            ],
+            });
+        });
+        this.setState({
+            messages,
         });
     }
 
-    onSend(messages = []) {
-        this.setState(previousState => ({
-            messages: GiftedChat.append(previousState.messages, messages)
-        }));
+    // adds new message to DB
+    addMessage() {
+        const message = this.state.messages[0];
+        this.referenceChatMessages.add({
+            _id: message._id,
+            uid: this.state.uid,
+            createdAt: message.createdAt,
+            text: message.text || null,
+            user: message.user,
+        });
     }
 
+    // adds new message to messages array in state
+    onSend(messages = []) {
+        this.setState(previousState => ({
+            messages: GiftedChat.append(previousState.messages, messages),
+        }),
+            () => {
+                this.addMessage();
+            });
+    }
+
+    // custom styling for active user's message bubble
     renderBubble(props) {
         return (
             <Bubble
@@ -51,8 +128,7 @@ class Chat extends Component {
     }
 
     render() {
-        const { name, backgroundColour } = this.props.route.params;
-        this.props.navigation.setOptions({ title: name });
+        const { backgroundColour } = this.props.route.params;
 
         return (
             <View style={[styles.container, { backgroundColor: backgroundColour }]}>
@@ -60,9 +136,7 @@ class Chat extends Component {
                     renderBubble={this.renderBubble.bind(this)}
                     messages={this.state.messages}
                     onSend={messages => this.onSend(messages)}
-                    user={{
-                        _id: 1
-                    }}
+                    user={this.state.user}
                 />
                 {/* Android keyboard fix */}
                 {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null
